a2960e91548f8ad3d5ab561b32747f419cb4bbb6
nurrbb <enurb320@gmail.com>
2025-11-23 17:03:44 +0300

    Add circle module and index file using module.exports and destructuring

---
+++ Circle/circle.js
@@ -0,0 +1,13 @@
+
+function circleArea(r) {
+  return Math.PI * r * r;
+}
+
+function circleCircumference(r) {
+  return 2 * Math.PI * r;
+}
+
+module.exports = {
+  circleArea,
+  circleCircumference,
+};
---
+++ Circle/index.js
@@ -0,0 +1,6 @@
+const { circleArea, circleCircumference } = require('./circle');
+
+const r = 5;
+
+console.log(`Yarıçapı ${r} olan dairenin alanı: ${circleArea(r)}`);
+console.log(`Yarıçapı ${r} olan dairenin çevresi: ${circleCircumference(r)}`);
